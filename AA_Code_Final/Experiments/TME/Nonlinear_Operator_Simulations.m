19bd0ebf7a742f3d491fbcd8d7c9a1bb41ed4ca6
Casey Garner <caseygarner10@gmail.com>
2025-07-25 22:42:04 -0500

    Update Nonlinear_Operator_Simulations.m

--- AA_Code_Final/Experiments/TME/Nonlinear_Operator_Simulations.m
+++ AA_Code_Final/Experiments/TME/Nonlinear_Operator_Simulations.m
@@ -4,8 +4,8 @@
 % with the TME model. This performs all of the numerical WITHOUT loading any 
 % the numerical results to reduce computational time. 
 %
-% Due to the randomness of the initializations these experiments behavior
-% simiarly to the figures presented in the paper. 
+% Note, the run times will vary from machine to machine. So, the box-plots
+% will not look exactly the same. 
 % 
 % To generate the figures in the paper run: 
 %     Nonlinear_Operation_Simulations_SHORT.m
